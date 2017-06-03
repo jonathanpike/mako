03789b860a920e199e2ac04f5fdb3769535a58a3
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2017-06-03 16:57:21 -0400

    Add some helpers for the rendered HTML documents

---
+++ lib/mako/view_helpers.rb
@@ -0,0 +1,15 @@
+module ViewHelpers
+  # Returns today's date in Day, Date Month Year format
+  #
+  # @return [String]
+  def today
+    Time.now.strftime('%A, %d %B %Y')
+  end
+
+  # Returns the current time in month day year hour:minute:second format
+  #
+  # @return [String]
+  def last_updated
+    Time.now.strftime('%m %b %Y %H:%M:%S')
+  end
+end
