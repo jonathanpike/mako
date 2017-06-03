94f6c8110aae06e3229561842e7973ee5f60ce90
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2017-06-03 16:59:31 -0400

    Add Configuration class to load YAML config from disk and provide a place to access it

---
+++ lib/mako/configuration.rb
@@ -0,0 +1,45 @@
+require 'yaml'
+
+module Mako
+  class Configuration
+    DEFAULT_CONFIGURATION = { 'source_templates' => File.expand_path('templates', File.dirname(__FILE__)),
+                              'destination' => File.expand_path('site/', Dir.pwd),
+                              'subscription_list' => File.expand_path('subscriptions.xml', Dir.pwd),
+                              'theme' => 'simple',
+                              'sanitize_images' => true,
+                              'config_file' => ''
+                            }.freeze
+
+    include FileOpenUtil
+
+    # Loads default config file and attempts to merge in any user settings.
+    # Creates a new instance of Mako::Configuration.
+    #
+    # @param [String]
+    # @return [Mako::Configuration]
+    def self.load(file)
+      begin
+        user_config_yaml = load_resource(file)
+      rescue SystemCallError
+        config = DEFAULT_CONFIGURATION
+        return new(config)
+      end
+      user_config = YAML.load(user_config_yaml)
+      user_config['config_file'] = file
+      config = DEFAULT_CONFIGURATION.merge(user_config)
+      new(config)
+    end
+
+    attr_reader :source_templates, :theme, :destination, :sanitize_images,
+                :subscription_list, :config_file
+
+    def initialize(args)
+      @source_templates = args.fetch('source_templates')
+      @theme = args.fetch('theme')
+      @destination = args.fetch('destination')
+      @sanitize_images = args.fetch('sanitize_images')
+      @subscription_list = args.fetch('subscription_list')
+      @config_file = args.fetch('config_file')
+    end
+  end
+end
---
+++ test/configuration_test.rb
@@ -0,0 +1,20 @@
+require_relative 'test_helper'
+
+class ConfigurationTest < Minitest::Test
+  def test_works_without_configuration_file
+    config = Mako::Configuration.load('')
+    assert_instance_of Mako::Configuration, config
+    assert_equal 'simple', config.theme
+    assert config.sanitize_images
+    assert_equal '', config.config_file
+  end
+
+  def test_works_with_configuration_file
+    config_file = File.expand_path('config.yaml', File.dirname(__FILE__))
+    config = Mako::Configuration.load(config_file)
+    assert_instance_of Mako::Configuration, config
+    assert_equal 'complex', config.theme
+    refute config.sanitize_images
+    assert_equal config_file, config.config_file
+  end
+end
