eb3d555a08c5cd0af6cad516b3eee6f8534178c7
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2017-06-03 16:57:53 -0400

    Add Writer class to write output of renderers

---
+++ lib/mako/writer.rb
@@ -0,0 +1,16 @@
+module Mako
+  class Writer
+    attr_reader :renderer, :destination
+
+    def initialize(args)
+      @renderer = args.fetch(:renderer)
+      @destination = args.fetch(:destination)
+    end
+
+    def write
+      File.open(destination, 'w+', encoding: 'utf-8') do |f|
+        f.write(renderer.render)
+      end
+    end
+  end
+end
