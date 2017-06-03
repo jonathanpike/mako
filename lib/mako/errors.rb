5a0af2049abe89178a9d666933212e96a1b103cc
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2017-06-03 16:58:41 -0400

    Add Errors class to deal with any errors in the execution of the render loop

---
+++ lib/mako/errors.rb
@@ -0,0 +1,23 @@
+module Mako
+  class Errors
+    attr_accessor :messages
+
+    def initialize
+      @messages = []
+    end
+
+    # Add an error message to the messages array
+    #
+    # @param [String]
+    def add_error(msg)
+      messages << msg
+    end
+
+    # Predicate method to see if there are any error messages
+    #
+    # @return [Boolean]
+    def any?
+      messages.count > 0
+    end
+  end
+end
