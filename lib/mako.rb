7c6dbc2f925cd248fb6181a281e2654acb13e118
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2017-06-04 13:27:48 -0400

    Require all of the appropriate files, provide global variables under the Mako namespace

--- lib/mako.rb
+++ lib/mako.rb
@@ -1,5 +1,39 @@
-require "mako/version"
+require 'time'
+require 'erb'
+require 'logger'
+require 'faraday'
+require 'feedjira'
+require 'nokogiri'
+require 'json'
+require 'sass'
+
+require_relative 'mako/core_ext'
+require_relative 'mako/errors'
+require_relative 'mako/file_open_util'
+require_relative 'mako/view_helpers'
+require_relative 'mako/configuration'
+require_relative 'mako/subscription_list_parser'
+require_relative 'mako/logger_formatter'
+require_relative 'mako/feed'
+require_relative 'mako/article'
+require_relative 'mako/feed_requester'
+require_relative 'mako/feed_constructor'
+require_relative 'mako/html_renderer'
+require_relative 'mako/sass_renderer'
+require_relative 'mako/writer'
+require_relative 'mako/core'
+require_relative 'mako/cli'
 
 module Mako
-  # Your code goes here...
+  def self.logger
+    @logger ||= Logger.new(STDOUT)
+  end
+
+  def self.config
+    @config ||= Configuration.load(File.expand_path('config.yaml', Dir.pwd))
+  end
+
+  def self.errors
+    @errors ||= Errors.new
+  end
 end
