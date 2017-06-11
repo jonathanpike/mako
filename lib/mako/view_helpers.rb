d431457cd2287e89316aed3642f7044585ea508b
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2017-06-11 09:33:07 -0400

    Create new quick_nav view helper

--- lib/mako/view_helpers.rb
+++ lib/mako/view_helpers.rb
@@ -14,4 +14,15 @@ module ViewHelpers
   def last_updated
     Time.now.strftime('%d %b %Y %H:%M:%S')
   end
+
+  # Returns a string with anchor tag links to each Feed generated on the page
+  #
+  # @return [String]
+  def quick_nav(feeds)
+    feeds.select { |feed| feed.articles.size.positive? }
+         .each_with_index.inject("") do |string, (feed, index)|
+      string += "<a href='#feed-#{index}' class='quick-nav-item'>#{feed.title} <div class='circle'>#{feed.articles.size}</div></a>"
+      string
+    end
+  end
 end
