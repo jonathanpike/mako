5c8b154b77d67252814ac864d062006e97a63714
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2017-07-12 12:25:02 -0400

    Add SubscriptionListWriter to deal with updating subscription lists after subscribe command

---
+++ lib/mako/subscription_list_writer.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+module Mako
+  class SubscriptionListWriter
+    include FileOpenUtil
+
+    attr_reader :feeds, :destination
+
+    def initialize(args)
+      @feeds = args.fetch(:feeds)
+      @destination = args.fetch(:destination)
+    end
+
+    # Appends the new subscriptions to the subscription list and
+    # writes the results out to the file.
+    def append_and_write
+      contents = append_and_render
+      File.open(destination, 'w+', encoding: 'utf-8') do |f|
+        f.write(contents)
+      end
+    end
+
+    private
+
+    # @private
+    # Returns the rendered string for the correct file type.
+    #
+    # @return [String]
+    def append_and_render
+      loaded_list = SubscriptionListParser.new(list: destination)
+      case File.extname destination
+      when '.xml' || '.opml'
+        render_opml(loaded_list)
+      when '.json'
+        render_json(loaded_list)
+      when '.txt'
+        render_txt(loaded_list)
+      end
+    end
+
+    # Append feeds to current subscription list and return a string separated
+    # by \n characters
+    #
+    # @return [String]
+    def render_txt(list)
+      (list.parse + feeds).join("\n")
+    end
+
+    # Append feeds to current subscription list and return a JSON array
+    #
+    # @return [String]
+    def render_json(list)
+      (list.parse + feeds).to_json
+    end
+
+    # Append feeds to current subscription list and return XML document
+    #
+    # @return [String]
+    def render_opml(list)
+      document = Nokogiri::XML(list.load_list)
+      feeds.each do |feed_url|
+        node = "<outline xmlUrl='#{feed_url}' />\n"
+        document.xpath("//outline[@text='Subscriptions']").last.add_child node
+      end
+      formatted_no_decl = Nokogiri::XML::Node::SaveOptions::FORMAT +
+                          Nokogiri::XML::Node::SaveOptions::NO_DECLARATION
+      document.to_xml(encoding: 'utf-8', save_with: formatted_no_decl)
+    end
+  end
+end
---
+++ test/mako/subscription_list_writer_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+
+class SubscriptionListWriterTest < Minitest::Test
+  def setup
+    @xml_subscription = File.expand_path('../helper_files/subscriptions.xml', File.dirname(__FILE__))
+    @json_subscription = File.expand_path('../helper_files/subscriptions.json', File.dirname(__FILE__))
+    @txt_subscription = File.expand_path('../helper_files/subscriptions.txt', File.dirname(__FILE__))
+    @original = %w[https://jonathanpike.net/feed.xml https://daringfireball.net/feeds/main https://marco.org/rss
+                   http://blog.joshualongbrake.com/rss http://furbo.org/feed/ http://leancrew.com/all-this/feed/
+                   http://rss.desiringgod.org http://gilandamy.blogspot.com/feeds/posts/default?alt=rss
+                   https://www.challies.com/feed http://karbonbased.io/index.xml https://panic.com/blog/feed/
+                   https://www.schneems.com/feed.xml http://rideearth.net/feed/ http://inessential.com/xml/rss.xml
+                   http://scripting.com/feed.xml https://www.speedshop.co/feed.xml https://tenderlovemaking.com/atom.xml]
+    @expected = %w[https://jonathanpike.net/feed.xml https://daringfireball.net/feeds/main https://marco.org/rss
+                   http://blog.joshualongbrake.com/rss http://furbo.org/feed/ http://leancrew.com/all-this/feed/
+                   http://rss.desiringgod.org http://gilandamy.blogspot.com/feeds/posts/default?alt=rss
+                   https://www.challies.com/feed http://karbonbased.io/index.xml https://panic.com/blog/feed/
+                   https://www.schneems.com/feed.xml http://rideearth.net/feed/ http://inessential.com/xml/rss.xml
+                   http://scripting.com/feed.xml https://www.speedshop.co/feed.xml https://tenderlovemaking.com/atom.xml
+                   https://m.signalvnoise.com/feed http://blog.testdouble.com/index.xml https://512pixels.net/feed/]
+    @additional_feeds = %w[https://m.signalvnoise.com/feed http://blog.testdouble.com/index.xml https://512pixels.net/feed/]
+  end
+
+  def test_json_file_append_and_render
+    actual = Mako::SubscriptionListWriter.new(feeds: @additional_feeds, destination: @json_subscription).send(:append_and_render)
+    assert_equal @expected.to_json, actual
+  end
+
+  def test_txt_file_append_and_render
+    actual = Mako::SubscriptionListWriter.new(feeds: @additional_feeds, destination: @txt_subscription).send(:append_and_render)
+    assert_equal @expected.join("\n"), actual
+  end
+
+  def test_xml_file_append_and_render
+    actual = Mako::SubscriptionListWriter.new(feeds: @additional_feeds, destination: @xml_subscription).send(:append_and_render)
+    expected = Mako::SubscriptionListParser.new(list: File.expand_path('../helper_files/subscriptions_updated.xml', File.dirname(__FILE__))).load_list
+    assert_equal expected, actual
+  end
+end
